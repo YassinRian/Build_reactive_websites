4d763bc678b33714c03c6fa4c0826d3c1dc6db6b
Yassin <yassinrian@hotmail.com>
2022-01-21 00:56:33 +0100

    piglatinify

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,23 @@
-import { fromEvent } from 'rxjs';
-import { map, takeUntil } from 'rxjs/operators';
+import { fromEvent, from } from "rxjs";
+import { map, mergeMap, reduce } from 'rxjs/operators';
 
-let draggable: HTMLElement = document.querySelector('#draggable');
-
-let mouseDown$ = fromEvent<MouseEvent>(draggable, 'mousedown') ;
-let mouseMove$ = fromEvent<MouseEvent>(document, 'mousemove');
-let mouseUp$ = fromEvent<MouseEvent>(document, 'mouseup');
+let textbox = document.querySelector('#textbox');
+let results = document.querySelector('#results');
 
 
-mouseDown$.subscribe( () => {
-    mouseMove$.pipe(
-        map( event => {
-            event.preventDefault();
-            return {
-                x: event.clientX,
-                y: event.clientY
-            };
-        }),
-        takeUntil(mouseUp$)
-    ).subscribe(
-        pos => {
-            draggable.style.cursor = 'none';
-            draggable.style.left = (pos.x - 50) + 'px';
-            draggable.style.top = (pos.y - 50) + 'px';
-        });
-});
+function pigLatinify (word: string) {
+    if (word.length > 2) {
+        return word.slice(1) + '-' + word[0] + 'ay'
+    }
+    return word;
+}
 
-mouseUp$.subscribe( () => draggable.style.cursor = '' );
+
+fromEvent<any>(textbox, 'keyup').pipe(
+    map(event => event.target.value), // gives us a sentence of all the words
+    mergeMap( word =>
+        from(word.split(/\s+/)).pipe( // due to mergeMap we keep the sentence instead of having an array of words, so now we can split the sentence based on spaces
+           map(pigLatinify),
+            reduce( (firstW, nextW) => firstW + ' ' + nextW, '' )
+        ))
+).subscribe( w => results.innerHTML = w );
